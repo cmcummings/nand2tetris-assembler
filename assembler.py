d8c9aa58649affcadd1d9e761a6efda2c691f303
connor <cmcummings2003@gmail.com>
2019-02-19 13:16:55 -0600

    Update comp

--- assembler.py
+++ assembler.py
@@ -50,7 +50,6 @@ print("Created/opened file:", translated_file_path)
 
 
 # Translation
-binary_lines = []
 for l, line in enumerate(file_to_translate):
     # Remove comments and whitespace from the line
     line = line.split("//")[0].strip()
@@ -59,7 +58,7 @@ for l, line in enumerate(file_to_translate):
     instruction_type = 1 # 0=a, 1=c
     v = 0b0 # A-instruction address
 
-    a, c, d, j = "0", ["0"] * 6, ["0"] * 3, ["0"] * 3
+    a, c, d, j = "0", "000000", ["0"] * 3, ["0"] * 3
     
     # Skip empty lines
     if len(line) == 0: continue
@@ -87,15 +86,34 @@ for l, line in enumerate(file_to_translate):
         if ";" in line: # dest, jump
             jump = line.split(";")[1]
         comp = line
-        if dest is not None:
-            comp = comp.split("=")[1]
-            if jump is not None:
-                comp = comp.split(";")[0]
+        temp = comp.split("=")
+        if len(temp) > 1:
+            comp = temp[1]
+        temp = comp.split(";")
+        if len(temp) > 0:
+            comp = temp[0]
 
         print("dest:", dest, "| comp:", comp, "| jump:", jump)
 
         # Parse comp
-        
+        if comp == "0":
+            c = "101010"
+        elif comp == "1":
+            c = "111111"
+        elif comp == "-1":
+            c = "111010"
+        elif comp == "D":
+            c = "001100"
+        elif comp == "A":
+            c = "110000"
+        elif comp == "!D":
+            c = "001101"
+        elif comp == "!A":
+            c = "110001"
+        elif comp == "-D":
+            c = "001111"
+        elif comp == "-A":
+            c = "110011" # TODO finish hardcoding cause im a bad coder
 
         # Parse dest
         if dest is not None:
@@ -106,31 +124,32 @@ for l, line in enumerate(file_to_translate):
         # Parse jump
         if jump is not None:
             if jump[0:2] == "JG": 
-                j[2] = "1"
-                if jump[2] == "E": j[1] = "1"
+                if jump[2] == "T" or jump[2] == "E": 
+                    j[2] = "1"
+                    if jump[2] == "E": j[1] = "1"
+                else:
+                    raise Exception("Invalid jump command: " + jump)
             elif jump[0:2] == "JL":
-                j[0] = "1"
-                if jump[2] == "E": j[1] = "1"
+                if jump[2] == "T" or jump[2] == "E": 
+                    j[0] = "1"
+                    if jump[2] == "E": j[1] = "1"
+                else:
+                    raise Exception("Invalid jump command: " + jump)
             elif jump == "JMP": j[0:3] = ["1"] * 3
             elif jump == "JNE": 
                 j[0], j[2] = "1", "1"
             elif jump == "JEQ": 
-                j[1] = "1"        
+                j[1] = "1"
+            else:
+                raise Exception("Invalid jump command: " + jump)
 
     # Construct the instruction
     if instruction_type == 0:
-        print(str(v))
-        binary_lines.append("0" + v)
+        translated_file.write("0" + v + "\n")
     elif instruction_type == 1:
-        binary_lines.append("111" + a + "".join(c) + "".join(d) + "".join(j))
+        translated_file.write("111" + a + c + "".join(d) + "".join(j) + "\n")
 
     print("\n")
-
-
-# Write the binary lines to the binary file
-for line in binary_lines:
-    translated_file.write(line + "\n")
-
 
 # Close opened files
 file_to_translate.close()
